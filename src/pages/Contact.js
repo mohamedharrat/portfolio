a7356a8edea3f9f69885d26e82f0bebdb5d83b05
unknown <mohamedh.harrat@gmail.com>
2022-04-28 22:35:49 +0000

    contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,8 +3,45 @@ import Navigation from "../components/Navigation";
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact">
       <Navigation />
+      <section class="contactContainer">
+        <form action="#">
+          <h2>Nous Contacter</h2>
+          <div class="form-grid">
+            <div>
+              <label for="name">Nom</label>
+              <input type="text" id="name" />
+            </div>
+            <div>
+              <label for="email">Email</label>
+              <input type="text" id="email" />
+            </div>
+            <div class="textarea">
+              <label for="message">Message</label>
+              <textarea id="message"></textarea>
+            </div>
+          </div>
+          <input type="submit" class="button" />
+        </form>
+        <div className="infoContainer">
+          <div className="info">
+            <i className="fas fa-phone"></i>
+            <h2>Phone</h2>
+            <p>+21275861485</p>
+          </div>
+          <div className="info">
+            <i class="fas fa-envelope"></i>
+            <h2>E-mail</h2>
+            <p>mohamedh.harrat@gmail.com</p>
+          </div>
+          <div className="info">
+            <i class="fas fa-flag"></i>
+            <h2>Address</h2>
+            180 avenue mohamed V mohammedia
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
